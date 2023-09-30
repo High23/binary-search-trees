dd68df892f4955a3bd5108d0d1f832b57a05af5e
High23 <warenasb2@gmail.com>
2023-09-29 19:02:19 -0700

    Added delete and find functions to Tree class

--- index.js
+++ index.js
@@ -35,10 +35,79 @@ class Tree {
         }
     }
     delete(value) {
-
+        if (typeof value !== 'number') return 'The inputted value must be a number';
+        let nodeToBeReplaced = this.root;
+        let parentNode;
+        while (true) {
+            if (nodeToBeReplaced.right && nodeToBeReplaced.right.data === value || nodeToBeReplaced.left && nodeToBeReplaced.left.data === value) {
+                parentNode = nodeToBeReplaced;
+            }
+            if (value > nodeToBeReplaced.data) {
+                nodeToBeReplaced = nodeToBeReplaced.right
+            } else if (value < nodeToBeReplaced.data) {
+                nodeToBeReplaced = nodeToBeReplaced.left
+            } else {
+                break;
+            }
+        }
+        let replacementNode;
+        let replacementNodeParent;
+        if (nodeToBeReplaced.right !== null) {
+            replacementNode = nodeToBeReplaced.right
+            // 1 child node case
+            if (replacementNode.right === null && replacementNode.left === null) {
+                nodeToBeReplaced.data = replacementNode.data;
+                nodeToBeReplaced.right = null
+                return;
+            }
+            // 2 children node case
+            while (true) {
+                if (replacementNode.left === null) {
+                    break;
+                }
+                replacementNodeParent = replacementNode
+                replacementNode = replacementNode.left;
+            }
+            nodeToBeReplaced.data = replacementNode.data;
+            if (replacementNodeParent.left === replacementNode) {
+                replacementNodeParent.right = null;
+                replacementNodeParent.left = replacementNode.right;
+            } else if (replacementNodeParent.right === replacementNode) {
+                replacementNodeParent.left = null;
+                replacementNodeParent.right = replacementNode.left;
+            }
+            return;
+        } else if (nodeToBeReplaced.left !== null) {
+            replacementNode = nodeToBeReplaced.left
+            if (parentNode.left === nodeToBeReplaced) {
+                parentNode.left = replacementNode;
+            } else if (parentNode.right === nodeToBeReplaced) {
+                parentNode.right = replacementNode;
+            }
+            return
+        }
+        // leaf node case
+        if (parentNode.left === nodeToBeReplaced) {
+            parentNode.left = null;
+        } else if (parentNode.right === nodeToBeReplaced) {
+            parentNode.right = null;
+        }
+    }
+    find(value) {
+        if (typeof value !== 'number') return 'The inputted value must be a number';
+        let root = this.root
+        while (true) {
+            if (value > root.data) {
+                root = root.right
+            } else if (value < root.data) {
+                root = root.left
+            } else {
+                break;
+            }
+        }
+        console.log(root)
     }
 }
-
 
 function mergeSort(arr) {
     if (arr.length < 2) return arr;
@@ -86,8 +155,6 @@ function sortedArrayToBST(sortedArray) {
     }
 }
 
-let BST = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
         return;
@@ -101,11 +168,17 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
 };
 
+let BST = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
 BST.insert(10)
 BST.insert(11)
 BST.insert(6)
 BST.insert(0)
 BST.insert(2)
+BST.insert(212)
+BST.insert(7000)
 
+
+BST.delete(2)
 prettyPrint(BST.root)
 // [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
